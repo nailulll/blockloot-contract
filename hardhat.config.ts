89e6e35e3b5d9c91c99628a6c3285a4d3c008e19
ilul4530 <ilul4530@gmail.com>
2025-02-06 03:28:31 +0000

    setup deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY! || "";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    },
+    hardhat: {
+      chainId: 1337,
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,8 +2,7 @@ import hre from "hardhat";
 
 async function main() {
   const BlockLoot = await hre.ethers.getContractFactory("BlockLoot");
-  const blockLoot = await BlockLoot.deploy();
-
+  const blockLoot = await BlockLoot.deploy(process.env.SEPOLIA_ADDRESS || "");
   console.log(`Deployed to ${blockLoot.target}`);
 }
 
